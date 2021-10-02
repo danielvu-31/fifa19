a7c05bad54f1246ac28804cb7f243a9046d577cd
vudh3 <duchongvu01@gmail.com>
2021-10-01 22:38:50 -0400

    Add bayesian results and ckpt

---
+++ scripts/lightgbm_bayesian_final.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#CUDA_VISIBLE_DEVICES=1 \
+python -m train \
+    --data_folder data \
+    --best_config_folder tune_result/best_params.json \
+    --ckpt_folder best_ckpt \
+    --model lightgbm \
+    --tuning bayesian
